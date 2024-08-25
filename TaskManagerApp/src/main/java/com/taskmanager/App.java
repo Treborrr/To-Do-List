7d7b92e02fc6c468182f3d883779caedf8542944
Robert Alvarado <100499519+Treborrr@users.noreply.github.com>
2024-08-25 04:21:14 +0000

    probando la clase principal

--- TaskManagerApp/src/main/java/com/taskmanager/App.java
+++ TaskManagerApp/src/main/java/com/taskmanager/App.java
@@ -1,13 +1,11 @@
 package com.taskmanager;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ){
+        Task task = new Task("Aprendiendo Java", "Para mejorar mis conocimientos en Java creare el proyecto 'Task Manager'");
+    System.out.println(task);
+    task.setComplete(true);
+    system.out.println("Despues de completar la tarea: "); 
+    system.out.println(task);  
     }
 }
